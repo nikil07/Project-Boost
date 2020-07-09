ef72123f860241468d564abae6120fecd11c6a33
Nikhil Kumar <nikilkumar0810@gmail.com>
2020-07-09 18:40:05 +0530

    Project_Boost L41 Added Rocket color, movement and engine sound.

---
+++ Assets/Rocket.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rocketRigitBody;
+    AudioSource rocketAudioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rocketRigitBody = GetComponent<Rigidbody>();
+        rocketAudioSource = GetComponent<AudioSource>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        processInput();
+    }
+
+    private void processInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Thursting");
+            rocketRigitBody.AddRelativeForce(Vector3.up);
+            if (!rocketAudioSource.isPlaying)
+                rocketAudioSource.Play();
+        }
+        else {
+            rocketAudioSource.Stop();
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        } else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
